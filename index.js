597fdf22934850cd59ab61b9c62dc53a75b241a4
shwetapund <pundshweta2020@gmail.com>
2022-12-27 23:08:19 -0800

    sync function/process examples

--- index.js
+++ index.js
@@ -5,10 +5,35 @@ setTimeout(()=>{
     console.log('Printing After  Seconds')
 }, 5000)
 
-*/
+*************************************************************************************stop
 setTimeout(()=>{
     console.log('Printing After  Seconds')
 }, 6000)
 
+**********************************************************************
+
+console.log("person 1")
+console.log("person 2")
+console.log("person 3") //Sync function
+console.log("person 4")
 
 
+console.log("person 1")
+setTimeout(()=>{
+    console.log("Person 2") //Async Function It is not way to async but  for example  hein sirf.
+}, 3000)
+
+console.log("Person 3")
+
+*/
+
+function fun1(){
+    console.log('Fun 1')
+}
+
+function fun2(){
+    console.log('Fun 2')
+}
+
+fun1()
+fun2()
