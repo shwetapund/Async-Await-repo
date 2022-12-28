ff1be89d37dd69a3d8754b2c095c8b5cfe5fb559
shwetapund <pundshweta2020@gmail.com>
2022-12-27 22:21:21 -0800

    index fileis added

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+setTimeouut...............
+
+setTimeout(()=>{
+    console.log('Printing After  Seconds')
+}, 5000)
+
+*/
+setTimeout(()=>{
+    console.log('Printing After  Seconds')
+}, 6000)
+
+
+
